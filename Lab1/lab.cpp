4be1d7228f3fbc1a6ac50016776fee3a27044871
Seba <sebastian.acevedo.13@sansano.usm.cl>
2020-01-14 20:10:12 -0300

    1.1

--- Lab1/lab.cpp
+++ Lab1/lab.cpp
@@ -1,6 +1,6 @@
 #include <iostream> 
 #include <math.h>
-
+#include <time.h>
 
 float* CPUEuler(float t_0, float y_0, float delta_t){
 	int n=10/delta_t;
@@ -8,17 +8,38 @@ float* CPUEuler(float t_0, float y_0, float delta_t){
 	for (int i=0;i<n;i++){
 		y[i]=y_0;
 		for (int j=0;j<i;j++){
-			int t_j=t_0+j*delta_t;
+			float t_j=t_0+j*delta_t;
 			y[i]+=delta_t*(9*pow(t_j,2)-4*t_j+5);
 		}
 	}
 	return y;
 }
+/*float* CPUEuler2(float t_0, float y_0, float delta_t){
+	int n=10/delta_t;
+	float y[n];
+	y[0]=y_0;
+	for (int i=1;i<n;i++){
+		float t_i=t_0+i*delta_t;
+		y[i]=y[i-1]+delta_t*(9*pow(t_i,2)-4*t_i+5);
+	}
+	return y;
+}
+*/
 
 int main(){ 
-	for (int i=0;i<10/0.1;i++){
-		printf("%f\n",CPUEuler(0,4,0.1)[i]);	
+	clock_t start, end;
+	for(int i=1;i<7;i++){
+		float delta_t=pow(10,-i);
+		start=clock();
+		CPUEuler(0,4,delta_t);
+		end=clock();
+		double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf("%f\n",cpu_time_used);
 	}
-	return 0; 
+	
+	/*for (int i=0;i<10/0.1;i++){
+		printf("%f ",CPUEuler(0,4,0.1)[i]);
+	}
+	return 0; */
 } 
 
